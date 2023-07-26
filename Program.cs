2b089cff71ecd3bfd6cc17b7025ff2b047085806
kishan.kumar4@mphasis.com <kishan.kumar4@mphasis.com>
2023-07-26 17:12:44 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RainbowSchools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"E:\Project1.9\1) student.txt";
+            try
+            {
+
+                Console.WriteLine("Student Data:");
+                Console.WriteLine("-------------");
+                string fileContent = File.ReadAllText(filePath);
+                Console.WriteLine(fileContent);
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
